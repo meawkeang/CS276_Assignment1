7c868dacddd671c002ff5d8141e9f03e363d0cb1
Cleophus Robinson IV <CleoR41@gmail.com>
2015-04-14 17:16:25 -0700

    Rewrote the index part

--- robinsca/cs276-pa1/src/cs276/assignments/BasicIndex.java
+++ robinsca/cs276-pa1/src/cs276/assignments/BasicIndex.java
@@ -1,6 +1,10 @@
 package cs276.assignments;
 
 import java.nio.channels.FileChannel;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasicIndex implements BaseIndex {
 
@@ -15,5 +19,17 @@ public class BasicIndex implements BaseIndex {
 	@Override
 	public void writePosting(FileChannel fc, PostingList p) {
 		//System.out.println(p);
+		Integer termID = p.getTermId();
+		List<Integer> postings = p.getList();
+		int numPostings = postings.size();
+		try{
+			fc.write(ByteBuffer.allocate(4).putInt(termID));
+			fc.write(ByteBuffer.allocate(4).putInt(numPostings));
+			for(int i = 0; i < numPostings; i++){
+				fc.write(ByteBuffer.allocate(4).putInt(postings.get(i)));
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }

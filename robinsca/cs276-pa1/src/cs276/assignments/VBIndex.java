e106c30186fbb12e94cdfe8b95a98400109008ca
Cleophus Robinson IV <CleoR41@gmail.com>
2015-04-16 02:25:38 -0700

    Encode number works

--- robinsca/cs276-pa1/src/cs276/assignments/VBIndex.java
+++ robinsca/cs276-pa1/src/cs276/assignments/VBIndex.java
@@ -1,21 +1,73 @@
 package cs276.assignments;
 
 import java.nio.channels.FileChannel;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VBIndex implements BaseIndex {
 
 	@Override
 	public PostingList readPosting(FileChannel fc) {
-		/*
-		 * Your code here
-		 */
+		try{
+			if(fc.position() >= fc.size()) return null;
+			ByteBuffer creds = ByteBuffer.allocate(2*4);
+			fc.read(creds);
+			int termID = creds.getInt(0);
+			int numPostings = creds.getInt(4);
+			ArrayList<Integer> postings = new ArrayList<Integer>(numPostings);
+			ByteBuffer docs = ByteBuffer.allocate(numPostings*4);
+			fc.read(docs);
+			for(int i = 0; i < numPostings; i++){
+				postings.add(new Integer(docs.getInt(i*4)));
+			}
+			PostingList pl = new PostingList(termID,postings);
+			return pl;
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public void writePosting(FileChannel fc, PostingList p) {
-		/*
-		 * Your code here
-		 */
+		//System.out.println(p);
+		Integer termID = p.getTermId();
+		List<Integer> postings = p.getList();
+		int numPostings = postings.size();
+		try{
+			ByteBuffer bf = ByteBuffer.allocate(2*4 + numPostings*4);
+			bf.clear();
+			bf.putInt(termID);
+			bf.putInt(numPostings);
+			for(int i = 0; i < numPostings; i++){
+				bf.putInt(postings.get(i));
+			}
+			bf.flip();
+			fc.write(bf);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
+
+	private byte[] vbEncodeNumber(int number){
+		ArrayList<Byte> finalByteList = new ArrayList<Byte>();
+		while(true){
+			finalByteList.add(0,new Byte((byte)(number%128)));
+			if(number < 128) break;
+			number = number/128;
+		}
+		Byte lastByte = finalByteList.get(finalByteList.size()-1);
+		byte terminatedByte = (byte)((int)lastByte.byteValue() + (int)128);
+		finalByteList.set(finalByteList.size()-1,new Byte(terminatedByte));
+		//Copt byte arraylist to byte array
+		byte[] byteAr = new byte[finalByteList.size()];
+		for(int i = 0; i < finalByteList.size(); i++){
+			byteAr[i] = (finalByteList.get(i)).byteValue();
+		}
+		return byteAr;
+	}
+
+
 }
--- robinsca/cs276-pa1/toy_example/index.sh
+++ robinsca/cs276-pa1/toy_example/index.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-java -Xmx400M -cp $SCRIPTPATH/../classes cs276.assignments.Index Basic $1 $2
+java -Xmx400M -cp $SCRIPTPATH/../classes cs276.assignments.Index VB $1 $2

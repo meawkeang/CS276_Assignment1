011e09bc12ef3124c8efa43aaa59f531bbd475f2
Cleophus Robinson IV <CleoR41@gmail.com>
2015-04-16 04:09:27 -0700

    Compression index is giving me problems

--- robinsca/cs276-pa1/src/cs276/assignments/VBIndex.java
+++ robinsca/cs276-pa1/src/cs276/assignments/VBIndex.java
@@ -15,14 +15,24 @@ public class VBIndex implements BaseIndex {
 			ByteBuffer creds = ByteBuffer.allocate(2*4);
 			fc.read(creds);
 			int termID = creds.getInt(0);
-			int numPostings = creds.getInt(4);
-			ArrayList<Integer> postings = new ArrayList<Integer>(numPostings);
-			ByteBuffer docs = ByteBuffer.allocate(numPostings*4);
+			int numBytes = creds.getInt(4);
+			ArrayList<Integer> postings = new ArrayList<Integer>();
+			ByteBuffer docs = ByteBuffer.allocate(numBytes);
 			fc.read(docs);
-			for(int i = 0; i < numPostings; i++){
-				postings.add(new Integer(docs.getInt(i*4)));
+			byte[] byteArray = docs.array();
+			int number = 0;
+			for(int i = 0; i < numBytes; i++){
+				byte b = byteArray[i];
+				if(b < 128){
+					number = number*128 + b;
+				}else{
+					number = number*128 + b - 128;
+					postings.add(new Integer(number));
+					number = 0;
+				}
 			}
 			PostingList pl = new PostingList(termID,postings);
+			//System.out.println(pl);
 			return pl;
 		}catch(IOException e){
 			e.printStackTrace();
@@ -35,15 +45,13 @@ public class VBIndex implements BaseIndex {
 		//System.out.println(p);
 		Integer termID = p.getTermId();
 		List<Integer> postings = p.getList();
-		int numPostings = postings.size();
+		ArrayList<Byte> byteList = vbEncodeStream(postings);
 		try{
-			ByteBuffer bf = ByteBuffer.allocate(2*4 + numPostings*4);
+			ByteBuffer bf = ByteBuffer.allocate(2*4 + byteList.size());
 			bf.clear();
 			bf.putInt(termID);
-			bf.putInt(numPostings);
-			for(int i = 0; i < numPostings; i++){
-				bf.putInt(postings.get(i));
-			}
+			bf.putInt(new Integer(byteList.size()));
+			bf.put(arListToByteAr(byteList));
 			bf.flip();
 			fc.write(bf);
 		}catch(IOException e){
@@ -51,7 +59,20 @@ public class VBIndex implements BaseIndex {
 		}
 	}
 
-	private byte[] vbEncodeNumber(int number){
+	private ArrayList<Byte> vbEncodeStream(List<Integer> docIds){
+		//System.out.println(docIds);
+		int initialDoc = (docIds.get(0)).intValue();
+		ArrayList<Byte> byteAr = vbEncodeNumber(initialDoc);
+		int prevDoc = initialDoc;
+		for(int i = 1; i < docIds.size(); i++){
+			int nextDoc = docIds.get(i);
+			byteAr.addAll(vbEncodeNumber(nextDoc-prevDoc));
+			prevDoc = nextDoc;
+		}
+		return byteAr;
+	}
+
+	private ArrayList<Byte> vbEncodeNumber(int number){
 		ArrayList<Byte> finalByteList = new ArrayList<Byte>();
 		while(true){
 			finalByteList.add(0,new Byte((byte)(number%128)));
@@ -61,10 +82,14 @@ public class VBIndex implements BaseIndex {
 		Byte lastByte = finalByteList.get(finalByteList.size()-1);
 		byte terminatedByte = (byte)((int)lastByte.byteValue() + (int)128);
 		finalByteList.set(finalByteList.size()-1,new Byte(terminatedByte));
-		//Copt byte arraylist to byte array
-		byte[] byteAr = new byte[finalByteList.size()];
-		for(int i = 0; i < finalByteList.size(); i++){
-			byteAr[i] = (finalByteList.get(i)).byteValue();
+		return finalByteList;
+	}
+
+	private byte[] arListToByteAr(ArrayList<Byte> byteList){
+		//Copy byte arraylist to byte array
+		byte[] byteAr = new byte[byteList.size()];
+		for(int i = 0; i < byteList.size(); i++){
+			byteAr[i] = (byteList.get(i)).byteValue();
 		}
 		return byteAr;
 	}

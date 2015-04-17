d5bb69eb6a7e08a237f56aa97eec06419160f3e5
Cleophus Robinson IV <CleoR41@gmail.com>
2015-04-17 08:56:07 -0700

    Submitted

--- dev_work/cs276-pa1/toy_example/query.sh
+++ dev_work/cs276-pa1/toy_example/query.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-java -Xmx200M -cp $SCRIPTPATH/../classes cs276.assignments.Query Basic $1
+java -Xmx200M -cp $SCRIPTPATH/../classes cs276.assignments.Query VB $1
